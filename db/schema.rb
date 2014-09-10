74b541a1172b0edfce254b2343d814146bd432f1
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 10:11:43 -0400

    Add author to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910134808) do
+ActiveRecord::Schema.define(version: 20140910140608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string "title", null: false
+    t.string "title",  null: false
+    t.string "author", null: false
   end
 
 end
