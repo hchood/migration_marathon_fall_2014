00fa3ef16a6b31575e439a7586a3d7fe5721b78f
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 10:17:12 -0400

    Add favorite to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910140608) do
+ActiveRecord::Schema.define(version: 20140910141409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string "title",  null: false
-    t.string "author", null: false
+    t.string  "title",                    null: false
+    t.string  "author",                   null: false
+    t.boolean "favorite", default: false
   end
 
 end
