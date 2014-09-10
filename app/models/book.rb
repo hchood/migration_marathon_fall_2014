31ffae298f53720f8f8623920e719363ff6260f8
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 11:11:23 -0400

    Add associations with categorization model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ActiveRecord::Base
   has_many :checkouts
+  has_many :categorizations
+  has_many :categories, through: :categorizations
 
   validates :title, presence: true
   validates :author, presence: true
