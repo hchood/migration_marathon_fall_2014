4375ee5dea37a1573cdd791fbf7644265ab46057
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 11:21:20 -0400

    Add full_name instance method

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -6,4 +6,8 @@ class Reader < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
