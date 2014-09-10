9b49ac0deb9079136eb1b108b5bc2480714074ee
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 17:11:55 -0400

    Delete broken migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910152637) do
+ActiveRecord::Schema.define(version: 20140910152144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20140910152637) do
     t.integer  "book_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "reader_id",  null: false
+    t.string   "reader",     null: false
   end
 
   create_table "readers", force: true do |t|
