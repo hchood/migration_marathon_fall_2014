c7d1aa17dd6457f9cd558a6fd01a0ffd26336eaa
Helen Hood <helen.c.hood@gmail.com>
2014-09-10 17:16:34 -0400

    Add reader_id to checkouts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910152144) do
+ActiveRecord::Schema.define(version: 20140910211459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140910152144) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "reader",     null: false
+    t.integer  "reader_id"
   end
 
   create_table "readers", force: true do |t|
